1ffaa78b587a8fe84dc78d7410a63738c400cee9
Killian HARROIS <130766904+Kikiiiyy@users.noreply.github.com>
2024-03-18 17:29:09 +0100

    Update script.js
    Added bubble sort and bogo sort (bogo not fully working, problem on the display).

--- script.js
+++ script.js
@@ -75,3 +75,73 @@ async function selectionSort(){
 		}
 	}
 }
+
+async function bubbleSort() {
+	for (let i = 0; i < tab.length; i++) {
+ 
+        // Last i elements are already in place  
+        for (let j = 0; j < (tab.length - i - 1); j++) {
+ 
+            // Checking if the item at present iteration 
+            // is greater than the next iteration
+            if (tab[j] > tab[j + 1]) {
+ 
+                // If the condition is true
+                // then swap them
+                let temp = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = temp;
+                deleteArray();
+		showArray();
+                await sleep(10*(100/(inputSpeed.value)));
+            }
+        }
+    }
+}
+
+function arrayEqual(tab1, tab2){
+	if(tab1.length!=tab2.length){
+		return false;
+	}
+	for(let i=0;i<tab1.length;i++){
+		if(tab1[i]!=tab2[i]){
+		return false;
+		}
+	}
+	return true;
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+async function bogoSort(){
+	let sortedArray=[...tab];
+	sortedArray.sort();
+	while(!(arrayEqual(tab, sortedArray))){
+                await sleep(10*(100/(inputSpeed.value)));
+		shuffle(tab);
+                deleteArray();
+		showArray();
+                await sleep(10*(100/(inputSpeed.value)));
+	}
+}
+
+function sortArray(){
+	let sort=document.getElementById("sortSelect").value;
+	switch(sort){
+		case "selection":
+			selectionSort();
+			break;
+		case "bubble":
+			bubbleSort();
+			break;
+		case "bogo":
+			bogoSort();
+			break;
+	}
+}
